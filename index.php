19319b0dd5f50c2eb592b89836f648c443b69653
Dnx453 <157989112+Dnx453@users.noreply.github.com>
2024-07-02 17:34:10 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,49 +1,66 @@
 <?php
-require_once '../Mysqli.php';
-require_once '../user_banco.php';
+include 'conexao.php';
 
+//$enviar = $pdo->prepare("INSERT INTO usuarios(nome, email, cpf, senha) VALUES (:n,:e,:c,:s)");
+//$enviar->bindValue(":n","Felipe");
+//$enviar->bindValue(":e","Fm751062@gmail.com");
+//$enviar->bindValue(":c","08186577904");
+//$enviar->bindValue(":s","Felipe23");
+//$enviar->execute();
+
+//$del = $pdo->prepare("DELETE FROM usuarios WHERE id = :id");
+//$id = 1;
+//$del->bindValue(":id",$id);
+//$del->execute();
+
+//$upd = $pdo->prepare("UPDATE usuarios SET cpf = :cpf WHERE id = :id");
+//$id = 2;
+//$cpf = '08423090981';
+//$upd->bindValue(":id",$id);
+//$upd->bindValue(":cpf",$cpf);
+//$upd->execute();
+
+$slc = $pdo->prepare("SELECT * FROM usuarios WHERE id = :id");
+$id = 2;
+$slc->bindValue(":id",$id);
+$slc->execute();
+$resul = $slc->fetch(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
-<html lang="pt_BR">
+<html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pagina de login - Troia Leiloes</title>
 </head>
 <body>
-    <?php
-    $result = $p->buscarDados();
-    ?>
-    <table>
-     <tr>
-        <th>NOME</th>
+<form action="" method="post">
+    <label for="nome">Email ou Cpf:</label>
+    <input type="text" id="nome" name="nome" placeholder="Digite seu email ou cpf" require>
+    
+    <label for="email">Senha:</label>
+    <input type="password" id="senha" name="senha" placeholder="Digite sua senha">
+    
+    <input type="submit" value="Enviar">
+</form>
+
+<table>
+    <tr>
+        <th>Id</th>
+        <th>Nome</th>
         <th>Email</th>
         <th>Cpf</th>
         <th>Senha</th>
-        <th>Operações</th>
-     <tr>
-     <tr>
-     <?php
-    if(count($result) > 0)
-    {
-        for ($i=0; $i < count($result); $i++) { 
-            echo "<tr>";
-            foreach ($result[$i] as $key => $value) {
-                if($key != "id")
-                {
-                    echo "<td>".$value."</td>";
-                }
-              }?>
-              <td><a href="">EDITAR </a><a href="">Excluir</a><td>
-              <?php
-              echo "</tr>";
-        }
-    }
-    ?>
-     
     </tr>
+    <tr>
+      <?php foreach ($resul as $key => $value) {
+        echo "<td>" .$value . "</td>";
+      }
+      ?>
 
+    </tr>
+    
+</table>
 
-    </table>
 </body>
 </html>
